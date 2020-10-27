bf188a4b63ab93797b50980e14f1558c6dd7aa8a
lukeliuli <lukeliuli@163.com>
2020-10-27 10:28:39 +0800

    Signed-off-by: lukeliuli <lukeliuli@163.com>

--- testMiscDriver1/testRead.py
+++ testMiscDriver1/testRead.py
@@ -1,8 +1,7 @@
-
-# 打开文件进行读取
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 fh = open("/dev/mymiscled", "r")
-# 根据大小读取文件内容
-print('输出来自 read() 方法\n', fh.read(2048))
-# 关闭文件
-fh.close()
-
+str1 = fh.read(10)
+print "读取的字符串是 : ", str1
+# 关闭打开的文件
+fo.close()
