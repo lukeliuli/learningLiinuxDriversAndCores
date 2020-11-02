7823985e2489e4bf7cd70e6a011b32db0d67f450
lukeliuli <lukeliuli@163.com>
2020-11-03 01:47:38 +0800

    Signed-off-by: lukeliuli <lukeliuli@163.com>

--- testCP210xPi/testRead.py
+++ testCP210xPi/testRead.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-fh = open("/dev/ttyUSB0", "r")
-str1 = fh.read(10)
-print("读取的字符串是 : ", str1,"\n")
-# 关闭打开的文件
-fh.close()
+
+import serial
+from time import sleep
+ 
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5) 
+ 
+print(ser.port)
+print(ser.baudrate)
+ 
+def recv(serial):  
+  while True:  
+    data =serial.read(42)  
+    if data == '':  
+      continue
+    else:
+      break
+    sleep(0.02) 
+  return data 
+ 
+while True:  
+  data =recv(ser) 
+  if data != '': 
+    print(data)
+    
