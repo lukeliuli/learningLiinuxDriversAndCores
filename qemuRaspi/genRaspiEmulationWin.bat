137752ef0d0f4d9bdaf0b70b9df0f33620a47388
lukeliuli <lukeliuli@163.com>
2022-01-15 00:46:29 +0800

    加入QEMU资料

---
+++ qemuRaspi/genRaspiEmulationWin.bat
@@ -0,0 +1,11 @@
+chcp 65001
+qemu-system-aarch64 ^
+-M raspi3b ^
+-kernel kernel7.img ^
+-sd lite.img ^
+-append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootwait" ^
+-dtb bcm2710-rpi-3-b.dtb  ^
+-m 1024M ^
+-nographic ^
+-netdev user,id=net0,hostfwd=tcp::5522-:22 ^
+-device usb-net,netdev=net0
---
+++ qemuRaspi/其他大文件在百度网盘.bat
@@ -0,0 +1,3 @@
+包括
+1.ultraoISO,用于ISO文件制作
+2.lite的镜像
